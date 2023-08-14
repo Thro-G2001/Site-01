f1037d705fb6890f251f2073e05d18a84ea67c12
François Géthro <133711656+Thro-G2001@users.noreply.github.com>
2023-08-14 14:15:43 -0500

    Add files via upload

---
+++ contacter.js
@@ -0,0 +1,5 @@
+let toujou = document.getElementById('conect');
+
+toujou.onclick = function(){
+  alert('Merci d\'avoir cliquer, mais il manque le langage PHP pour les suites nécessaires, utiliser plutôt la carte qui est dans la page service.  Merci.');
+};
---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('Hello World!');
+
+let autre = document.getElementById('web');
+
+autre.onclick = function(){
+  alert('Merci d\'avoir cliquer, mais ce site est en cours de conception. À plus😉');
+};
+ 
+ // autres code 
+ 
